d951972ce543623fdd7d60834db3222b86923b8e
Fidel Ernesto Alonso Ochoa <fidehlg89@gmail.com>
2022-05-07 17:40:35 -0400

    changed word to search

--- src/App.tsx
+++ src/App.tsx
@@ -35,12 +35,11 @@ const App = () => {
       <div className="flex flex-col justify-center items-center">
         <b>{process.env.REACT_APP_TITLE}</b>
         <div>
-          <button className="rounded-md" onClick={() => getPokemon()}>
+          <button className="px-4 py-3 text-white bg-blue-500 hover:bg-blue-400" onClick={() => getPokemon()}>
             Get List
           </button>
-          <span> | </span>
           <button
-            className="rounded-md"
+            className="px-4 py-3 text-white bg-gray-500 hover:bg-gray-400"
             onClick={() => {
               setPokes([]);
               setSearchedPokes([]);
@@ -55,7 +54,7 @@ const App = () => {
         <div className="p-4 relative mx-auto text-gray-600 flex justify-center">
           <input
             className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-            placeholder="Buscar"
+            placeholder="Search..."
             onChange={handleChange}
           />
         </div>
