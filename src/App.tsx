2af408787e07b8789e77ed9bb83ff0bcd6758eb3
Fidel Ernesto Alonso Ochoa <fidehlg89@gmail.com>
2022-05-07 15:27:43 -0400

    Adding some responsive

--- src/App.tsx
+++ src/App.tsx
@@ -52,23 +52,27 @@ const App = () => {
       </div>
 
       <div>
-        <div className="p-4 relative mx-auto text-gray-600">
+        <div className="p-4 relative mx-auto text-gray-600 flex justify-center">
           <input
             className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
             placeholder="Buscar"
             onChange={handleChange}
           />
         </div>
-        <div className="grid grid-cols-4 gap-4 p-4">
+        <div className="grid grid-cols-4 gap-4 px-4">
           {loading
             ? "Loading"
             : searchedList.length > 0
             ? searchedList.map((item: IPokemon, index) => (
-                <PokeCard pokemon={item} key={index} />
+                <div className="col-span-4 md:col-span-1 sm:col-span-2">
+                  <PokeCard pokemon={item} key={index} />
+                </div>
               ))
             : pokeList.length > 0
             ? pokeList.map((item: IPokemon, index) => (
-                <PokeCard pokemon={item} key={index} />
+                <div className="col-span-4 md:col-span-1 sm:col-span-2">
+                  <PokeCard pokemon={item} key={index} />
+                </div>
               ))
             : null}
         </div>
